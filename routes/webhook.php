daba0aece5b2fe850c802c056d5b445c4d01e90e
Vahagn <vahagn99ghukasyan@gmail.com>
2024-08-05 12:23:44 +0400

    add: add middleware to accept json in green api webhooks

--- routes/webhook.php
+++ routes/webhook.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\GreenApi\WebhookController;
+use App\Http\Middleware\SetJsonHeaderInGreenApiWebhook;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('webhooks')->as('webhooks.')->group(function () {
-    Route::post('greenapi', WebhookController::class)->name('greenapi');
+    Route::middleware(SetJsonHeaderInGreenApiWebhook::class)->post('greenapi', WebhookController::class)->name('greenapi');
 });
