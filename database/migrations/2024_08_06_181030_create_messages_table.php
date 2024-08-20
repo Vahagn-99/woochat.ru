fb38b3023623449af0da9198ecfcc6c9109613b6
Vahagn <vahagn99ghukasyan@gmail.com>
2024-08-20 11:01:59 +0000

    test amocrm install

--- database/migrations/2024_08_06_181030_create_messages_table.php
+++ database/migrations/2024_08_06_181030_create_messages_table.php
@@ -5,22 +5,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-
+return new class extends Migration{
     public function up(): void
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
-            $table->string('amo_message_id')->unique();
-            $table->string('whatsapp_message_id')->unique();
+            $table->string('amo_message_id');
+            $table->string('whatsapp_message_id');
 
-            $table->foreignIdFor(Chat::class, 'chat_id')
-                ->constrained()
-                ->references('id')
-                ->onUpdate('cascade')
-                ->onDelete('cascade');
+            $table->foreignIdFor(Chat::class, 'chat_id')->constrained()->references('id')->onUpdate('cascade')->onDelete('cascade');
 
             $table->timestamps();
+
+            $table->unique(['whatsapp_message_id', 'amo_message_id']);
         });
     }
 
