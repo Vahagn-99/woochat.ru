281c1c4250b08c7e1abf463af63739fae996dc69
Vahagn <vahagn99ghukasyan@gmail.com>
2024-08-21 10:41:19 +0000

    pusher install

--- routes/channels.php
+++ routes/channels.php
@@ -1,9 +1,8 @@
 <?php
 
 use App\Models\User;
-use App\Models\WhatsappInstance;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('instances.{instanceId}', function (User $user, int $instanceId) {
-    return $user->id === WhatsappInstance::query()->find($instanceId)->user_id;
+Broadcast::channel('instances.{instanceId}', function () {
+    return true;
 });
