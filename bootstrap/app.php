11e720d04cc9a36d4fc9dd5df34602bf6d4408f1
Vahagn <vahagn99ghukasyan@gmail.com>
2024-08-21 11:33:31 +0000

    pusher install

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -32,6 +32,7 @@ return Application::configure(basePath: dirname(__DIR__))->withRouting(web: [
         'graphql',
         'webhooks/*',
         'amocrm/*',
+        '*broadcasting/auth*',
     ]);
 
     $middleware->alias([
