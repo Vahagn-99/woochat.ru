9341845462a770e4cbe872517ff71203704cee60
Vahagn <vahagn99ghukasyan@gmail.com>
2024-08-21 12:16:36 +0000

    pusher install

--- database/migrations/2024_08_02_125511_create_whatsapp_instances_table.php
+++ database/migrations/2024_08_02_125511_create_whatsapp_instances_table.php
@@ -5,22 +5,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-
+return new class extends Migration{
     public function up(): void
     {
         Schema::create('whatsapp_instances', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->string('token')->unique();
-            $table->string('phone')->nullable()->unique();
+            $table->string('phone')->nullable();
             $table->string('status')->default(InstanceStatus::STARTING);
             $table->timestamp('created_at')->useCurrent();
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')
-                ->references('id')
-                ->on('users')
-                ->onUpdate('cascade')
-                ->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
