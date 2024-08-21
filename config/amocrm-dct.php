325ad7e3968a9b1317e51e696baf00de4031cfe3
Vahagn <vahagn99ghukasyan@gmail.com>
2024-08-21 08:13:25 +0000

    test amocrm install

--- config/amocrm-dct.php
+++ config/amocrm-dct.php
@@ -47,7 +47,7 @@ return [
         'client_id' => env('DCT_AMOCRM_CLIENT_ID', '99d7c33b-1d67-477b-9614-844a2eb3bcd7'),
         'domain' => env('DCT_AMOCRM_CLIENT_DOMAIN', 'tech8.amocrm.ru'),
         'client_secret' => env('DCT_AMOCRM_CLIENT_SECRET', 'zb3ZSIQ5BxcKGqYeFf44O14t84IqHjRNxeTEUN8VRB8poIix3Jm3ZBDauRlNrbYm'),
-        'redirect_url' => env('DCT_AMOCRM_REDIRECT_URL', 'https://united-gopher-amazing.ngrok-free.app/api/amocrm/dct/auth/callback'),
-        'account_delete' => env('DCT_AMOCRM_ACCOUNT_DELETE', 'https://united-gopher-amazing.ngrok-free.app/api/amocrm/widget/delete'),
+        'redirect_url' => env('DCT_AMOCRM_REDIRECT_URL', 'https://api.woochat.ru/api/amocrm/dct/auth/callback'),
+        'account_delete' => env('DCT_AMOCRM_ACCOUNT_DELETE', 'https://api.woochat.ru/api/amocrm/widget/delete'),
     ],
 ];
