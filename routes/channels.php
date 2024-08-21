4e9439eb19becebe46a67f3a5e2b32d8674ac5b0
Vahagn <vahagn99ghukasyan@gmail.com>
2024-08-21 11:38:46 +0000

    pusher install

--- routes/channels.php
+++ routes/channels.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Models\User;
+use App\Models\WhatsappInstance;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::routes(['middleware' => ['auth:sanctum']]);
 
-Broadcast::channel('instances.{instanceId}', function (User $user) {
-    return true;
+Broadcast::channel('instances.{instanceId}', function (User $user, $instanceId) {
+    return WhatsappInstance::query()->findOrFail($instanceId)->user_id === $user->id;
 });
