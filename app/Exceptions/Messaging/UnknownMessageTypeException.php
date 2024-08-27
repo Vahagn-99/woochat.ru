927de007aa46f9e4460c704419af6417d5c0ffb4
Vahagn <vahagn99ghukasyan@gmail.com>
2024-08-27 10:06:31 +0000

    fix messaging

--- app/Exceptions/Messaging/UnknownMessageTypeException.php
+++ app/Exceptions/Messaging/UnknownMessageTypeException.php
@@ -6,13 +6,13 @@ use App\Exceptions\ReportableException;
 
 class UnknownMessageTypeException extends ReportableException
 {
-    public function __construct(private readonly string $type, private readonly string $provider = 'amocrm')
+    public function __construct(private readonly string $type, private readonly string $provider = 'amochat')
     {
-        parent::__construct();
+        parent::__construct('Unknown message type: '.$this->type);
     }
 
     public function report(): void
     {
-        do_log("{$this->provider}/message-type-".now()->toDateTimeString())->error('Unknown message type: '.$this->type);
+        do_log("{$this->provider}/types-".now()->toDateTimeString())->error('Unknown message type: '.$this->type);
     }
 }
