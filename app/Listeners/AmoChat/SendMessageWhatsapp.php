dce0c733115bbf6bc738c80ef4c1cbac151655f0
Vahagn <vahagn99ghukasyan@gmail.com>
2024-08-30 11:55:48 +0000

    add redis queue, horizon

--- app/Listeners/AmoChat/SendMessageWhatsapp.php
+++ app/Listeners/AmoChat/SendMessageWhatsapp.php
@@ -47,7 +47,7 @@ class SendMessageWhatsapp implements ShouldQueue
      */
     public function handle(MessageReceived $event): void
     {
-        try {
+        //try {
 
             $amoMessageId = $event->payload['message']['message']['id'];
 
@@ -74,12 +74,12 @@ class SendMessageWhatsapp implements ShouldQueue
                 'record' => $record->toArray(),
                 'payload' => $messagePayload->toArray(),
             ]);
-        } catch (Exception|ModelNotFoundException $e) {
-            do_log("messaging/sent/amochat")->error($e->getMessage(), $event->payload);
-            $this->release();
-
-            return;
-        }
+        //} catch (Exception|ModelNotFoundException $e) {
+        //    do_log("messaging/sent/amochat")->error($e->getMessage(), $event->payload);
+        //    $this->release();
+        //
+        //    return;
+        //}
     }
 
     /**
