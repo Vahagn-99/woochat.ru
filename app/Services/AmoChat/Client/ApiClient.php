11b2edbcf118918057ad247e26a76a604dc297ef
Vahagn <vahagn99ghukasyan@gmail.com>
2024-09-08 18:40:27 +0000

    fix message sending

--- app/Services/AmoChat/Client/ApiClient.php
+++ app/Services/AmoChat/Client/ApiClient.php
@@ -114,9 +114,9 @@ class ApiClient implements ApiClientInterface
 
         if ($response->failed()) {
             $message = " ".PHP_EOL;
-            $message .= "error code: ".json_decode($response->body(), true)['error_code'].PHP_EOL ?? '';
-            $message .= "error type: ".json_decode($response->body(), true)['error_type'].PHP_EOL ?? '';
-            $message .= "error description: ".json_decode($response->body(), true)['error_description'].PHP_EOL ?? '';
+            $message .= "error code: ".json_decode($response->body(), true)['error_code'] ?? ' '.PHP_EOL;
+            $message .= "error type: ".json_decode($response->body(), true)['error_type'] ?? ' '.PHP_EOL;
+            $message .= "error description: ".json_decode($response->body(), true)['error_description'] ?? ' '.PHP_EOL;
             $message .= "request body: ".$requestBody;
 
             return [
