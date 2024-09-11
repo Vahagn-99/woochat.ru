ba697c100315a33bb6be8acfe8f3e5b9719863ef
Vahagn <vahagn99ghukasyan@gmail.com>
2024-09-11 08:07:44 +0000

    fix message sending

--- app/Listeners/Messengers/AmoChat/ConnectSource.php
+++ app/Listeners/Messengers/AmoChat/ConnectSource.php
@@ -10,6 +10,7 @@ use App\Events\Messengers\Whatsapp\SettingsSaved;
 use App\Models\Settings;
 use App\Models\User;
 use App\Services\AmoCRM\Core\Facades\Amo;
+use Exception;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
@@ -49,16 +50,14 @@ class ConnectSource implements ShouldQueue
         try {
             $api = Amo::domain($user->domain)->api()->sources();
 
-            $source = $api->get((new SourcesFilter())->setExternalIds([(string) $settings->instance_id]))?->first();
-
-            if ($source) {
+            try {
+                $source = $api->get((new SourcesFilter())->setExternalIds([(string) $settings->instance_id]))->first();
                 $source = $api->updateOne($source);
-            } else {
+            } catch (Exception) {
                 $source = $api->addOne($source);
             }
 
             $settings->source_id = $source->getId();
-
         } catch (AmoCRMApiException|AmoCRMoAuthApiException $e) {
             do_log('amochat/sources')->error($e->getMessage(), [
                 'data' => $e->getLastRequestInfo(),
