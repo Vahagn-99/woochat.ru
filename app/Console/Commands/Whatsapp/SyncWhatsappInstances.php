76df5bb5a621bcf6fb2f22a08b01719cf5eed040
Vahagn <vahagn99ghukasyan@gmail.com>
2024-09-11 11:18:18 +0400

    fix instace create

--- app/Console/Commands/Whatsapp/SyncWhatsappInstances.php
+++ app/Console/Commands/Whatsapp/SyncWhatsappInstances.php
@@ -50,7 +50,7 @@ class SyncWhatsappInstances extends Command
                 $exists->phone = null;
                 $exists->settings()->delete();
 
-                if ($exists->created_at->lessThan(now()->subHours(12))) {
+                if ($exists->created_at->lessThan(now()->subHours(12)) && $exists->user_id) {
                     do_log('crones/instances')->warning("Инстанс {$exists->id} откреплень от клиента {$exists->user_id}");
                     $exists->user_id = null;
                 }
