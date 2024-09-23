d0b935a3a51840dd0a490af69069263ede72b5e8
Vahagn <vahagn99ghukasyan@gmail.com>
2024-09-23 18:52:51 +0400

    fix

--- app/Console/Commands/Subscription/DailyCheckRenewal.php
+++ app/Console/Commands/Subscription/DailyCheckRenewal.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands\Subscription;
 
+use App\Enums\InstanceStatus;
 use App\Models\User as UserModel;
+use App\Models\WhatsappInstance;
 use Illuminate\Console\Command;
 
 class DailyCheckRenewal extends Command
@@ -27,12 +29,20 @@ class DailyCheckRenewal extends Command
     public function handle(): void
     {
         /** @var \Illuminate\Database\Eloquent\Collection<\App\Models\User> $users */
-        $users = UserModel::query()->withWhereHas('activeSubscription')->get();
+        $users = UserModel::query()->withWhereHas('subscriptions')->get();
 
         foreach ($users as $user) {
             $subscription = $user->activeSubscription;
 
             if ($subscription->expired_at->isPast()) {
+                $user->instances->each(function (WhatsappInstance $instance) {
+                    $instance->user_id = null;
+                    $instance->status = InstanceStatus::NOT_AUTHORIZED;
+                    $instance->phone = null;
+                    $instance->settings->delete();
+                    $instance->save();
+                });
+
                 $subscription->archive();
             }
         }
