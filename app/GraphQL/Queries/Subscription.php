97838d44dfdbbaccba19102d26aa6a3a1c78d4ab
Vahagn <vahagn99ghukasyan@gmail.com>
2024-09-23 19:08:27 +0400

    fix

--- app/GraphQL/Queries/Subscription.php
+++ app/GraphQL/Queries/Subscription.php
@@ -2,14 +2,16 @@
 
 namespace App\GraphQL\Queries;
 
+use Illuminate\Support\Carbon;
+
 final readonly class Subscription
 {
     /** @param array{} $args */
-    public function __invoke(null $_, array $args): \App\Models\Subscription
+    public function __invoke(null $_, array $args): string
     {
         /** @var \App\Models\User $user */
         $user = auth()->user();
 
-        return $user->activeSubscription?->expired_at;
+        return $user->activeSubscription?->expired_at?->format('Y-m-d');
     }
 }
