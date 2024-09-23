27a60e727ed0316380cbb5399e0c8c21f045355d
Vahagn <vahagn99ghukasyan@gmail.com>
2024-09-23 18:31:16 +0400

    fix

--- app/Http/Controllers/Api/UserWithSubscriptionController.php
+++ app/Http/Controllers/Api/UserWithSubscriptionController.php
@@ -33,7 +33,7 @@ class UserWithSubscriptionController extends Controller
                 $query->when(isset($filters['domain']), fn( $query) => $query->where('domain', 'LIKE', "%{$filters['domain']}%"));
                 $query->when(isset($filters['has_subscription']), fn( $query) => $filters['has_subscription'] ? $query->whereHas("activePaidSubscription") : $query->whereDoesntHave("activePaidSubscription"));
             })
-            ->orderByDesc('expired_at')
+            ->orderByDesc('activeSubscription.expired_at')
             ->orderByDesc('created_at')
             ->paginate(perPage:  $perPage, page:  $page);
 
