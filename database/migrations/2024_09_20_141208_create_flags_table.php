e646284eeaaee88b261d0ae4806b2056cfc90f36
Vahagn <vahagn99ghukasyan@gmail.com>
2024-09-25 15:39:17 +0400

    fix: custom fields list

--- database/migrations/2024_09_20_141208_create_flags_table.php
+++ database/migrations/2024_09_20_141208_create_flags_table.php
@@ -4,15 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration{
     public function up()
     {
         Schema::create('flags', function (Blueprint $table) {
             $table->id();
 
             $table->string('name');
-            $table->morphs('flaggable');
+            $table->string('flaggable_id');
+            $table->string('flaggable_type');
 
             $table->index(['name', 'flaggable_id', 'flaggable_type']);
 
