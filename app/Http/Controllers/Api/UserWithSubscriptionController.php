833e59237d65fa3288fc8414c8b494036dcbbe02
Vahagn <vahagn99ghukasyan@gmail.com>
2024-10-18 14:29:26 +0400

    Добавил инстансов в запрось подписек для админки

--- app/Http/Controllers/Api/UserWithSubscriptionController.php
+++ app/Http/Controllers/Api/UserWithSubscriptionController.php
@@ -33,16 +33,12 @@ class UserWithSubscriptionController extends Controller
             ->withCount(['whatsappInstances as current_instances_count'])
             ->where(function ($query) use ($filters) {
                 $query->when(isset($filters['id']), fn($query) => $query->where('id', 'LIKE', "%{$filters['id']}%"));
-                $query->when(
-                    isset($filters['domain']),
-                    fn($query) => $query->where('domain', 'LIKE', "%{$filters['domain']}%")
-                );
-                $query->when(
-                    isset($filters['has_subscription']),
-                    fn($query) => $filters['has_subscription'] ? $query->whereHas(
-                        "activePaidSubscription"
-                    ) : $query->whereDoesntHave("activePaidSubscription")
-                );
+                $query->when(isset($filters['domain']), fn($query) => $query->where('domain', 'LIKE', "%{$filters['domain']}%"));
+                $query->when(isset($filters['has_subscription']), fn($query) => $filters['has_subscription'] ? $query->whereHas("activePaidSubscription") : $query->whereDoesntHave("activePaidSubscription"));
+                $query->when(isset($filters['instance']), fn($query) => $query->whereHas('whatsappInstances',function ($query) use($filters) {
+                    $query->where("whatsapp_instances.id", "LIKE", "%{$filters['instance']}%");
+                    $query->orWhere("whatsapp_instances.phone", "LIKE", "%{$filters['instance']}%");
+                }));
             })
             ->withAggregate('activeSubscription', 'expired_at')
             ->orderByDesc(
