1457f0186739299cb1aca58102398b2b68122c42
Vahagn <vahagn99ghukasyan@gmail.com>
2024-11-18 14:29:52 +0000

    пофиксил удаление инстанса

--- app/Console/Commands/Whatsapp/DailyCheckBLockedInstances.php
+++ app/Console/Commands/Whatsapp/DailyCheckBLockedInstances.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands\Whatsapp;
 use App\Enums\InstanceStatus;
 use App\Models\WhatsappInstance;
 use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
 
 class DailyCheckBLockedInstances extends Command
 {
@@ -28,13 +29,11 @@ class DailyCheckBLockedInstances extends Command
     public function handle(): void
     {
         /** @var \Illuminate\Database\Eloquent\Collection<\App\Models\WhatsappInstance> $instances */
-        $instances = WhatsappInstance::whereBlocked()->get();
-        dd($instances->pluck('id')->toArray());
+        $instances = WhatsappInstance::whereBlocked()->where('blocked_at', '>=', Carbon::now()->addHours(6))->get();
+dd($instances->toArray());
         foreach ($instances as $instance) {
-            if ($instance->blocked_at->isBefore(now()->subHours(6))) {
-                $instance->status = InstanceStatus::NOT_AUTHORIZED;
-                $instance->save();
-            }
+            $instance->status = InstanceStatus::NOT_AUTHORIZED;
+            $instance->save();
         }
 
         do_log('crones/blocked-instances-daily-renewal')->info("Заблокированные инстансы были проверены на предмет восстановление.");
